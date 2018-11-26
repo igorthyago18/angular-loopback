3608868655f63a861017042b0993b664ad8c18cc
Igor Thyago De Sousa Silva <2016118050130@ifpi.int>
2018-11-26 11:46:46 -0300

    Método onSelected adicionado.

---
+++ client/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero";
+
+
+
+export const HEROES: Hero[] = [
+
+    {id: 11, name: "Thor"},
+    {id: 12, name: "Capitão América"},
+    {id: 13, name: "Homem de Ferro"}
+];
